8081e7af7d6f9b22cce384daf4293ce34be60f44
ipTIME GPL <iptime.gpl@guu.moe>
2018-05-23 09:34:20 +0900

    update

--- fs/bcmac/kernel_info.mk
+++ fs/bcmac/kernel_info.mk
@@ -1,4 +1,6 @@
 $(info include $(notdir $(lastword $(MAKEFILE_LIST))))
 
+KERNEL_PATH:=$(abspath ../../linux)
 KERNEL_FILENAME:=vmlinuz.9.10.178.27.fsrc
-KERNEL_PATH:=../../linux
+LINUX_PATH:=main/components/opensource/linux/linux-2.6.36
+COMPRESSED_DIR_PATH:=components/router/compressed
